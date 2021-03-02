262ffeb62af82bdb123f47c3aa709782c6cd376f
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-03-02 19:09:24 +0200

    Updated download folder hierarchy.

--- gather/satellite_download.py
+++ gather/satellite_download.py
@@ -17,15 +17,16 @@ class Download:
 
     def downlad_next_glacier(self):
         glacier = self.glacier_factory.get_next_glacier()
+        print(glacier)
 
         search = Search(url=STAC_API_URL,
                         bbox=glacier.get_bbox(),
                         query={'eo:cloud_cover': {'lt': 5}},
                         collections=COLLECTION)
         print('bbox search: %s items' % search.found())
-        self.download(search)
+        self.download(search, glacier)
 
-    def download(self, search):
+    def download(self, search, glacier):
         items = search.items(limit=10)
         print(items.summary())
         items.save("test_download.json")
@@ -33,5 +34,5 @@ class Download:
         loaded = ItemCollection.open("test_download.json")
         print(loaded.summary(['date', 'id', 'eo:cloud_cover']))
 
-        filenames = items.download_assets(['MTL', 'B3', 'B6'], filename_template='test_download/${date}/${id}')
+        filenames = items.download_assets(['MTL', 'B3', 'B6'], filename_template='test_download/' + glacier.get_wgi_id() + '/${date}/${id}')
         print(filenames)
