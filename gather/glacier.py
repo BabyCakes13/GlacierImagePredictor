56e4cacd0e833476d65908e5b4e822676e75ac08
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-03-25 13:34:05 +0200

    Updated glacier variables to private.
    Just neat picking.

--- gather/glacier.py
+++ gather/glacier.py
@@ -2,11 +2,12 @@ class Glacier:
     BBOX_SIZE = 0.00001
 
     def __init__(self, wgi_id, latitude, longitude, name=None):
-        self.wgi_id = wgi_id
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
+        self.__wgi_id = wgi_id
+        self.__name = name
+        self.__latitude = latitude
+        self.__longitude = longitude
         self.__number_scenes = 0
+        self.__bbox = []
         self.define_bounding_box()
 
     def define_bounding_box(self):
@@ -15,38 +16,38 @@ class Glacier:
 
         The bounding box is needed for satellite image search.
         """
-        min_longitude = float(self.longitude) - self.BBOX_SIZE
-        min_latitude = float(self.latitude) - self.BBOX_SIZE
-        max_longitude = float(self.longitude) + self.BBOX_SIZE
-        max_latitude = float(self.latitude) + self.BBOX_SIZE
+        min_longitude = float(self.__longitude) - self.BBOX_SIZE
+        min_latitude = float(self.__latitude) - self.BBOX_SIZE
+        max_longitude = float(self.__longitude) + self.BBOX_SIZE
+        max_latitude = float(self.__latitude) + self.BBOX_SIZE
 
-        self.bbox = [min_longitude,
-                     min_latitude,
-                     max_longitude,
-                     max_latitude]
+        self.__bbox = [min_longitude,
+                       min_latitude,
+                       max_longitude,
+                       max_latitude]
 
     def string_bbox(self):
         string_bbox = ""
-        for i, coordinate in enumerate(self.bbox):
+        for i, coordinate in enumerate(self.__bbox):
             string_bbox += str(coordinate)
-            if i != len(self.bbox) - 1:
+            if i != len(self.__bbox) - 1:
                 string_bbox += ","
         return string_bbox
 
     def get_bbox(self):
-        return self.bbox
+        return self.__bbox
 
     def get_wgi_id(self):
-        return self.wgi_id
+        return self.__wgi_id
 
     def get_latitude(self):
-        return self.latitude
+        return self.__latitude
 
     def get_longitude(self):
-        return self.longitude
+        return self.__longitude
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def set_number_scenes(self, number_scenes):
         self.__number_scenes = number_scenes
@@ -55,8 +56,8 @@ class Glacier:
         return self.__number_scenes
 
     def __str__(self):
-        return "Glacier[{}, {}, {}, {}, {}]".format(self.wgi_id,
-                                                    self.latitude,
-                                                    self.longitude,
+        return "Glacier[{}, {}, {}, {}, {}]".format(self.__wgi_id,
+                                                    self.__latitude,
+                                                    self.__longitude,
                                                     self.__number_scenes,
-                                                    self.name)
+                                                    self.__name)
