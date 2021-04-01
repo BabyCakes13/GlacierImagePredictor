3094f402490e6f7a2ffe89f91bd1f10d7aad5ddb
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-04-01 14:27:49 +0300

    Updated ROI crawl and crate.

--- process/crawlers/roi_crawler.py
+++ process/crawlers/roi_crawler.py
@@ -16,4 +16,17 @@ class RoiCrawler(Crawler):
 
         for scene in scenes:
             scene_id = SceneID(scene)
-            print(scene_id)
+            roi = self.create_roi(scene_id)
+
+            if not self.roi_appended(roi):
+                self.__rois.append(roi)
+                print("Added ROI {} for scene {}".format(roi, scene_id))
+
+    def create_roi(self, scene: SceneID) -> RegionOfInterest:
+        return RegionOfInterest(scene.path(), scene.row())
+
+    def roi_appended(self, roi) -> bool:
+        for r in self.__rois:
+            if roi == r:
+                return True
+        return False
