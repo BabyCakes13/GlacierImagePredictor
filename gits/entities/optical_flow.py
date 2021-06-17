030664fa8297dc5b61b2e51748ab03d447a4ae35
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-06-17 19:00:38 +0300

    Applied masks on OF input.

--- gits/entities/optical_flow.py
+++ gits/entities/optical_flow.py
@@ -14,6 +14,7 @@ class OpticalFlow(Image):
     def __init__(self, first_image: Image, second_image: Image):
         self.__first_image = first_image
         self.__second_image = second_image
+        self.__first_mask, self.__second_mask = self.mask_images()
         self.__optical_flow_image = None
         self.__optical_flow = None
 
@@ -22,23 +23,28 @@ class OpticalFlow(Image):
             self.__optical_flow_image = self.hsv_optical_flow()
         return self.__optical_flow_image
 
+    def mask_images(self) -> None:
+        first_mask = self.create_mask(self.__first_image.ndarray())
+        second_mask = self.create_mask(self.__second_image.ndarray())
+        return first_mask, second_mask
+
     def optical_flow(self) -> numpy.ndarray:
+        masked_first_image = numpy.ma.masked_array(self.__first_image.ndarray(), mask=self.__second_mask).filled(0)
+        masked_second_image = numpy.ma.masked_array(self.__second_image.ndarray(), mask=self.__first_mask).filled(0)
+
         if self.__optical_flow is None:
             logger.notice("Computing optical flow...")
-            self.__optical_flow = cv2.calcOpticalFlowFarneback(self.__first_image.ndarray(),
-                                                               self.__second_image.ndarray(),
-                                                               None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            self.__optical_flow = cv2.calcOpticalFlowFarneback(masked_first_image,
+                                                               masked_second_image,
+                                                               None, 0.5, 6, 15, 3, 5, 1.2, 0)
         return self.__optical_flow
 
     def hsv_optical_flow(self):
-        first_mask = self.create_mask(self.__first_image.ndarray())
-        second_mask = self.create_mask(self.__second_image.ndarray())
-
         optical_flow = self.optical_flow()
         magnitude, angle = cv2.cartToPolar(optical_flow[..., 0], optical_flow[..., 1])
 
-        magnitude = numpy.ma.masked_array(magnitude, mask=first_mask).filled(0)
-        magnitude = numpy.ma.masked_array(magnitude, mask=second_mask).filled(0)
+        magnitude = numpy.ma.masked_array(magnitude, mask=self.__first_mask).filled(0)
+        magnitude = numpy.ma.masked_array(magnitude, mask=self.__second_mask).filled(0)
 
         colored_clone = cv2.cvtColor(self.__first_image.ndarray(), cv2.COLOR_GRAY2BGR)
         hsv = numpy.zeros_like(colored_clone).astype(numpy.uint8)
@@ -46,7 +52,7 @@ class OpticalFlow(Image):
         hsv[..., 0] = angle * 180 / numpy.pi / 2
         hsv[..., 1] = 255
         hsv[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
-        # hsv[..., 2] = self.scale(hsv[..., 2], 10)
+        hsv[..., 2] = self.scale(hsv[..., 2], 3)
         # hsv[..., 2] = 255
 
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
